d7cce254350db68822ee2baa022766888a3f4541
Mimmolucio <domenico.lp@gmail.com>
2022-02-15 14:33:54 +0100

    Basic version including design finished and working

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Select Drawing Board
 drawBoard = document.querySelector('.drawBoard')
 
 
+// Variable Definitons
 let row = 16
 let totalPixels = row * row
 let width = 384 / row
 let squareDimension = width.toString() + "px"
 
 
-
+// Draw the Grid
 function drawGrid() {
     for (i = 0; i < totalPixels; i++) {
         div = document.createElement('div')
@@ -20,6 +22,7 @@ function drawGrid() {
     }
 }
 
+// Remove all the Childs of the DrawBoard - used to render a new Board
 function clearGrid() {
     while (drawBoard.firstChild) {
         drawBoard.removeChild(drawBoard.lastChild)
@@ -27,7 +30,7 @@ function clearGrid() {
 }
 
 
-
+// Add an Eventlistener to all pixels in the Board
 function selectPixels() {
     pixels = document.querySelectorAll('.drawDiv')
     pixels.forEach((pixel) => {
@@ -37,9 +40,11 @@ function selectPixels() {
     })
 }
 
+// Initial Setup of the Board (Default to 16x16)
 drawGrid()
 selectPixels()
 
+// Setup the buttons to change the pixel-density of the board
 const sizes = document.querySelectorAll('.size')
 sizes.forEach((siz) => {
     siz.addEventListener('click', () => {
@@ -51,4 +56,34 @@ sizes.forEach((siz) => {
         drawGrid()
         selectPixels()
     })
+})
+
+// function for the prompt
+function getInput() {
+    return parseInt(prompt("Please enter the desired pixels per row (1 - 64)"))
+
+}
+
+// Erase all Drawings and reset Board and ask user for size of board
+const clear = document.querySelector('.clear')
+clear.addEventListener('click', () => {
+    clearGrid()
+    let userInput = getInput()
+    while (userInput > 64 || userInput < 1 || isNaN(userInput)) {
+        userInput = getInput()
+    }
+    row = userInput
+    totalPixels = row * row
+    width = 384 / row
+    squareDimension = width.toString() + "px"
+
+    drawGrid()
+    selectPixels()
+})
+
+const erase = document.querySelector('.erase')
+erase.addEventListener('click', () => {
+    clearGrid()
+    drawGrid()
+    selectPixels()
 })
